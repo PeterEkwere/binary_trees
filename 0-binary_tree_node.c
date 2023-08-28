5916ad654c467031539cf7229ed80994ef7be236
PeterEkwere <peterekwere@icloud.com>
2023-08-28 12:30:12 +0100

    refactored code/fixed malloc size allocation

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -7,7 +7,7 @@
 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node = (binary_tree_t *)malloc(sizeof(binary_tree_t *));
+    binary_tree_t *new_node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
 
     if (new_node == NULL)
     {
