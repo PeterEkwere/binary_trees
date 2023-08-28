b9691c6a90e2fd9824e4e1daffb6c46acead3de7
PeterEkwere <peterekwere04@gmail.com>
2023-08-28 11:11:02 -0700

    fixed all betty errors for task 1 and 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,60 +1,49 @@
 #include "binary_trees.h"
 /**
- * binary_tree_insert_right - This is a function That inserts a nodes at the right
+ * binary_tree_insert_right - a function That inserts a nodes at the right
  * @parent: This is the Parent Node
  * @value: This is the newNode value
  * Return: A pointer to the node if success or NULL if failed
-*/
+ */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-    binary_tree_t *current = parent;
-    binary_tree_t *new_node;
-    
-    if (parent == NULL)
-    {
-        return (NULL);
-    }
+	binary_tree_t *current = parent;
+	binary_tree_t *new_node;
 
-    new_node = binary_tree_node(NULL, value);
-
-    if (new_node == NULL)
-    {
-        return (NULL);
-    }
-
-    if (value > current->n)
-    {
-        if (current->right == NULL)
-        {
-            current->right = new_node;
-            new_node->parent = current;
-        }
-        else
-        {
-            while (current->right != NULL && value > current->n)
-            {
-                current = current->right;
-            }
-            current->right = new_node;
-            new_node->parent = current;
-        }
-    }
-    else
-    {
-        if (current->left == NULL)
-        {
-            current->left = new_node;
-            new_node->parent = current;
-        }
-        else
-        {
-             while (current->left != NULL && value < current->n)
-            {
-                current = current->left;
-            }
-            current->right = new_node;
-            new_node->parent = current;
-        }
-    }
-    return new_node;
+	if (parent == NULL)
+		return (NULL);
+	new_node = binary_tree_node(NULL, value);
+	if (new_node == NULL)
+		return (NULL);
+	if (value > current->n)
+	{
+		if (current->right == NULL)
+		{
+			current->right = new_node;
+			new_node->parent = current;
+		}
+		else
+		{
+			while (current->right != NULL && value > current->n)
+				current = current->right;
+			current->right = new_node;
+			new_node->parent = current;
+		}
+	}
+	else
+	{
+		if (current->left == NULL)
+		{
+			current->left = new_node;
+			new_node->parent = current;
+		}
+		else
+		{
+			while (current->left != NULL && value < current->n)
+				current = current->left;
+			current->right = new_node;
+			new_node->parent = current;
+		}
+	}
+	return (new_node);
 }
