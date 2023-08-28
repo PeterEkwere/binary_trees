cda09a8ccf7504c5fff06cf95038ae99b108c6cf
PeterEkwere <peterekwere@icloud.com>
2023-08-28 19:44:13 +0100

    almost done with tree height function

--- binary_trees.h
+++ binary_trees.h
@@ -35,5 +35,5 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 int binary_tree_is_leaf(const binary_tree_t *node);
 int binary_tree_is_root(const binary_tree_t *node);
-
+size_t binary_tree_height(const binary_tree_t *tree);
 #endif
