1e9033144da7d0647c138d8814cee0709d194b13
PeterEkwere <peterekwere04@gmail.com>
2023-08-30 04:48:39 -0700

    fixed name error for task 7

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,13 +1,17 @@
 #include "binary_trees.h"
 /**
- * binary_tree_preorder - is a function that traverses a tree in pre-order
+ * binary_tree_inorder - is a function that traverses a tree in in-order
  * @tree: this is a pointer to the root node
  * @func: This is a pointer to a funciton to print a done
  * Return: Void
  */
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
+	if (tree == NULL)
+	{
+		return;
+	}
 	func(tree->n);
-	binary_tree_preorder(tree->left, (func)(tree->n));
-	binary_tree_preorder(tree->right, (func)(tree->n));
+	binary_tree_inorder(tree->left, func);
+	binary_tree_inorder(tree->right, func);
 }
